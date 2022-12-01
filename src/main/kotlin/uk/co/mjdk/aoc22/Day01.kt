de87ed48ae258f5d11ef9cfa2901202428a8b183
Matt Jadczak <m.jadczak@mwam.com>
2022-12-01 13:35:37 +0000

    Fix up code

--- src/main/kotlin/uk/co/mjdk/aoc22/Day01.kt
+++ src/main/kotlin/uk/co/mjdk/aoc22/Day01.kt
@@ -2,24 +2,19 @@ package uk.co.mjdk.aoc22
 
 import uk.co.mjdk.aoc.*
 
+fun useCalorieSums(receiver: (Sequence<Int>) -> Unit) {
+    aocInput(22, 1).useLines { lines ->
+        lines
+            .splitBy(String::isEmpty)
+            .map { it.sumOf(Integer::parseInt) }
+            .let(receiver)
+    }
+}
+
 fun main() {
     // Part 1
-    aocInput(22, 1).useLines { lines ->
-        lines
-            .splitBy(String::isEmpty)
-            .map { it.sumOf(Integer::parseInt) }
-            .max()
-            .let(::println)
-    }
+    useCalorieSums { it.max().let(::println) }
 
     // Part 2
-    aocInput(22, 1).useLines { lines ->
-        lines
-            .splitBy(String::isEmpty)
-            .map { it.sumOf(Integer::parseInt) }
-            .sortedDescending()
-            .take(3)
-            .sum()
-            .let(::println)
-    }
+    useCalorieSums { it.sortedDescending().take(3).sum().let(::println) }
 }
