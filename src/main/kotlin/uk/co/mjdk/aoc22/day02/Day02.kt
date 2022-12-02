b60b233b450f39dcc8e235e1999fb261dfaf26d5
Matt Jadczak <matt@mjdk.co.uk>
2022-12-02 19:07:33 +0000

    [2022] Day 2, Part 2

--- src/main/kotlin/uk/co/mjdk/aoc22/day02/Day02.kt
+++ src/main/kotlin/uk/co/mjdk/aoc22/day02/Day02.kt
@@ -26,6 +26,7 @@ enum class Sign {
 }
 
 fun main() {
+    // Part 1
     aocInput(22, 2).useLines { lines ->
         lines
             .map {
@@ -48,6 +49,59 @@ fun main() {
             }
             .map { (theirPlay, ourPlay) ->
                 val outcome = ourPlay.battle(theirPlay)
+                val shapeScore = when (ourPlay) {
+                    Sign.Rock -> 1
+                    Sign.Paper -> 2
+                    Sign.Scissors -> 3
+                }
+                val outcomeScore = when (outcome) {
+                    Result.Loss -> 0
+                    Result.Draw -> 3
+                    Result.Win -> 6
+                }
+                shapeScore + outcomeScore
+            }
+            .sum()
+            .let(::println)
+    }
+
+    // Part 2
+    aocInput(22, 2).useLines { lines ->
+        lines
+            .map {
+                assert(it.length == 3)
+                val theirPlay = when (it[0]) {
+                    'A' -> Sign.Rock
+                    'B' -> Sign.Paper
+                    'C' -> Sign.Scissors
+                    else -> throw IllegalArgumentException()
+                }
+
+                val outcome = when (it[2]) {
+                    'X' -> Result.Loss
+                    'Y' -> Result.Draw
+                    'Z' -> Result.Win
+                    else -> throw IllegalArgumentException()
+                }
+
+                theirPlay to outcome
+            }
+            .map { (theirPlay, outcome) ->
+                val ourPlay = when (outcome) {
+                    Result.Draw -> theirPlay
+                    Result.Win -> when (theirPlay) {
+                        Sign.Rock -> Sign.Paper
+                        Sign.Paper -> Sign.Scissors
+                        Sign.Scissors -> Sign.Rock
+                    }
+
+                    Result.Loss -> when (theirPlay) {
+                        Sign.Rock -> Sign.Scissors
+                        Sign.Paper -> Sign.Rock
+                        Sign.Scissors -> Sign.Paper
+                    }
+                }
+
                 val shapeScore = when (ourPlay) {
                     Sign.Rock -> 1
                     Sign.Paper -> 2
