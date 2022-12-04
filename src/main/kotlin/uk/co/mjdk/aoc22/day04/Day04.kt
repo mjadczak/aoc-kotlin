a6e8e7702d8850fc9f22f15743247c2b4c961792
Matt Jadczak <matt@mjdk.co.uk>
2022-12-04 19:20:06 +0000

    [2022] Day 4, Part 2

--- src/main/kotlin/uk/co/mjdk/aoc22/day04/Day04.kt
+++ src/main/kotlin/uk/co/mjdk/aoc22/day04/Day04.kt
@@ -16,6 +16,7 @@ fun IntRange.contains(other: IntRange): Boolean {
 }
 
 fun main() {
+    // Part 1
     aocInput(22, 4).useLines { lines ->
         lines
             .map { line ->
@@ -24,6 +25,23 @@ fun main() {
                 (from1..to1) to (from2..to2)
             }
             .filter { (r1, r2) -> r1.contains(r2) || r2.contains(r1) }
+            .count()
+            .let(::println)
+    }
+
+    // Part 2
+    aocInput(22, 4).useLines { lines ->
+        lines
+            .map { line ->
+                val res = linePat.matchEntire(line) ?: throw IllegalStateException()
+                val (from1, to1, from2, to2) = res.groupValues.asSequence().drop(1).map(Integer::parseInt).toList()
+                (from1..to1) to (from2..to2)
+            }
+            .filter { (r1, r2) ->
+                r1.contains(r2.first) || r1.contains(r2.last) || r2.contains(r1.first) || r2.contains(
+                    r1.last
+                )
+            }
             .count()
             .let(::println)
     }
