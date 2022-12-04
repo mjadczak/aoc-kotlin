48c2ff2827a95c6c30824b710315b18893ae644c
Matt Jadczak <matt@mjdk.co.uk>
2022-12-04 19:03:24 +0000

    [2022] Day 3, Part 2

--- src/main/kotlin/uk/co/mjdk/aoc22/day03/Day03.kt
+++ src/main/kotlin/uk/co/mjdk/aoc22/day03/Day03.kt
@@ -3,6 +3,7 @@ package uk.co.mjdk.aoc22.day03
 import uk.co.mjdk.aoc.aocInput
 
 fun main() {
+    // Part 1
     aocInput(22, 3).useLines { lines ->
         lines
             .map { line ->
@@ -13,6 +14,27 @@ fun main() {
                 val common = left.intersect(right)
                 assert(common.count() == 1)
                 common.first()
+            }
+            .map {
+                when (it) {
+                    in 'a'..'z' -> it - 'a' + 1
+                    in 'A'..'Z' -> it - 'A' + 27
+                    else -> throw IllegalStateException()
+                }
+            }
+            .sum()
+            .let(::println)
+    }
+
+    // Part 2
+    aocInput(22, 3).useLines { lines ->
+        lines
+            .map { it.toSet() }
+            .chunked(3)
+            .map { it.reduce { s1, s2 -> s1.intersect(s2) } }
+            .map {
+                assert(it.count() == 1)
+                it.first()
             }
             .map {
                 when (it) {
