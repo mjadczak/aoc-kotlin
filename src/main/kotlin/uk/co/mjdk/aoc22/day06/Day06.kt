2354ab2933c21763138fdd89152da970906455fc
Matt Jadczak <m.jadczak@mwam.com>
2022-12-08 10:27:22 +0000

    [2022] Day 6, Part 2

--- src/main/kotlin/uk/co/mjdk/aoc22/day06/Day06.kt
+++ src/main/kotlin/uk/co/mjdk/aoc22/day06/Day06.kt
@@ -2,10 +2,14 @@ package uk.co.mjdk.aoc22.day06
 
 import uk.co.mjdk.aoc.aocInput
 
+fun firstDistinctPosition(input: String, numDistinct: Int): Int =
+    input.asSequence().windowed(numDistinct).withIndex().first {
+        it.value.toSet().size == numDistinct
+    }.index + numDistinct
+
+
 fun main() {
     val input = aocInput(22, 6).use { it.readText() }.trim()
-    val first = input.asSequence().windowed(4).withIndex().first {
-        it.value.toSet().size == 4
-    }
-    println(first.index + 4)
+    println(firstDistinctPosition(input, 4))
+    println(firstDistinctPosition(input, 14))
 }
