49c486078b514d69e7ed4c8cc4840cf5d46705f9
Matt Jadczak <m.jadczak@mwam.com>
2022-12-12 14:23:00 +0000

    [2022] Day 10, Part 2

--- src/main/kotlin/uk/co/mjdk/aoc22/day10/Day10.kt
+++ src/main/kotlin/uk/co/mjdk/aoc22/day10/Day10.kt
@@ -20,33 +20,52 @@ sealed interface Instruction {
 
 data class CpuState(val cycleNum: Int, val xReg: Int)
 
-fun main() {
-    aocInput(22, 10).useLines { lines ->
-        val instructions = lines.map(Instruction::parse)
+fun calculateStates(instructions: Sequence<Instruction>): Sequence<CpuState> = sequence {
+    var cycle = 0
+    var xReg = 1
+    for (instruction in instructions) {
+        when (instruction) {
+            is Instruction.Noop -> {
+                cycle += 1
+                yield(CpuState(cycle, xReg))
+            }
 
-        val states = sequence {
-            var cycle = 0
-            var xReg = 1
-            for (instruction in instructions) {
-                when (instruction) {
-                    is Instruction.Noop -> {
-                        cycle += 1
-                        yield(CpuState(cycle, xReg))
-                    }
-
-                    is Instruction.Addx -> {
-                        cycle += 1
-                        yield(CpuState(cycle, xReg))
-                        cycle += 1
-                        yield(CpuState(cycle, xReg))
-                        xReg += instruction.operand
-                    }
-                }
+            is Instruction.Addx -> {
+                cycle += 1
+                yield(CpuState(cycle, xReg))
+                cycle += 1
+                yield(CpuState(cycle, xReg))
+                xReg += instruction.operand
             }
         }
+    }
+}
+
+fun part1() {
+    aocInput(22, 10).useLines { lines ->
+        val states = calculateStates(lines.map(Instruction::parse))
 
         val statesOfInterest = states.filter { (it.cycleNum + 20) % 40 == 0 }.toList()
         assert(statesOfInterest.last().cycleNum == 220)
         println(statesOfInterest.sumOf { it.cycleNum * it.xReg })
     }
+}
+
+fun part2() {
+    aocInput(22, 10).useLines { lines ->
+        val states = calculateStates(lines.map(Instruction::parse))
+
+        val pixels = states.map {
+            (-1..1).contains((it.cycleNum - 1) % 40 - it.xReg)
+        }
+
+        pixels.chunked(40).forEach { row ->
+            println(row.map { if (it) '#' else '.' }.joinToString(""))
+        }
+    }
+}
+
+fun main() {
+    part1()
+    part2()
 }
