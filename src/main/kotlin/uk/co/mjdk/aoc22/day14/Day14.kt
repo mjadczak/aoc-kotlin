b4d3be509cfdfdfebe86a2faefce019fd58f8bdc
Matt Jadczak <m.jadczak@mwam.com>
2022-12-14 13:25:20 +0000

    [2022] Day 14, Part 2

--- src/main/kotlin/uk/co/mjdk/aoc22/day14/Day14.kt
+++ src/main/kotlin/uk/co/mjdk/aoc22/day14/Day14.kt
@@ -27,45 +27,90 @@ enum class CellContents {
 
 // Lazy day today: manually hardcode table sizes, forgo range optimisation stuff
 
-fun main() {
-    aocInput(22, 14).useLines { lines ->
-        val grid = List(600) { MutableList(200) { CellContents.Nothing } }
-        fun cellAt(pos: Pos): CellContents = grid[pos.x][pos.y]
-        fun setAt(pos: Pos, contents: CellContents) {
-            grid[pos.x][pos.y] = contents
-        }
+class Grid() {
+    private val grid: List<MutableList<CellContents>> = List(800) { MutableList(200) { CellContents.Nothing } }
+    var maxY: Int = -1
+        private set
 
-        lines.forEach { line ->
-            val tuples = line.split(" -> ").map {
-                val (x, y) = it.split(",")
-                Pos(x.toInt(), y.toInt())
-            }
-            assert(tuples.size >= 2)
-            tuples.asSequence().windowed(2).forEach { (from, to) ->
-                (from..to).forEach {
-                    setAt(it, CellContents.Rock)
-                }
-            }
-        }
-
-        val sandStart = Pos(500, 0)
-        var sandCounter = 0
-        outer@ while (true) {
-            var sandPos = sandStart
-            while (true) {
-                val nextPos = sandPos.fallCandidates().firstOrNull { cellAt(it).isEmpty() }
-                if (nextPos == null) {
-                    setAt(sandPos, CellContents.Sand)
-                    break
-                }
-                if (nextPos.y == grid[0].size - 1) {
-                    // we're at the bottom of the screen
-                    break@outer
-                }
-                sandPos = nextPos
-            }
-            sandCounter += 1
-        }
-        println(sandCounter)
+    operator fun get(pos: Pos): CellContents = grid[pos.x][pos.y]
+    operator fun set(pos: Pos, contents: CellContents) {
+        grid[pos.x][pos.y] = contents
+        maxY = max(maxY, pos.y)
     }
+
+    fun xCount(): Int = grid.size
+    fun yCount(): Int = grid[0].size
+}
+
+fun getGrid(): Grid = aocInput(22, 14).useLines { lines ->
+    val grid = Grid()
+    lines.forEach { line ->
+        val tuples = line.split(" -> ").map {
+            val (x, y) = it.split(",")
+            Pos(x.toInt(), y.toInt())
+        }
+        assert(tuples.size >= 2)
+        tuples.asSequence().windowed(2).forEach { (from, to) ->
+            (from..to).forEach {
+                grid[it] = CellContents.Rock
+            }
+        }
+    }
+
+    return grid
+}
+
+fun part1() {
+    val grid = getGrid()
+
+    val sandStart = Pos(500, 0)
+    var sandCounter = 0
+    outer@ while (true) {
+        var sandPos = sandStart
+        while (true) {
+            val nextPos = sandPos.fallCandidates().firstOrNull { grid[it].isEmpty() }
+            if (nextPos == null) {
+                grid[sandPos] = CellContents.Sand
+                break
+            }
+            if (nextPos.y == grid.yCount() - 1) {
+                // we're at the bottom of the screen
+                break@outer
+            }
+            sandPos = nextPos
+        }
+        sandCounter += 1
+    }
+    println(sandCounter)
+}
+
+fun part2() {
+    val grid = getGrid()
+    val floorY = grid.maxY + 2
+
+    // add floor
+    (0 until grid.xCount()).forEach { x ->
+        grid[Pos(x, floorY)] = CellContents.Rock
+    }
+
+    val sandStart = Pos(500, 0)
+    var sandCounter = 0
+    while (grid[sandStart].isEmpty()) {
+        var sandPos = sandStart
+        while (true) {
+            val nextPos = sandPos.fallCandidates().firstOrNull { grid[it].isEmpty() }
+            if (nextPos == null) {
+                grid[sandPos] = CellContents.Sand
+                break
+            }
+            sandPos = nextPos
+        }
+        sandCounter += 1
+    }
+    println(sandCounter)
+}
+
+fun main() {
+    part1()
+    part2()
 }
