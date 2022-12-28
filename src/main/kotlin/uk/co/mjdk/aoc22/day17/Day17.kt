7cf6fa8a583fd141903bafd4f70f0eec72e67b03
Matt Jadczak <matt@mjdk.co.uk>
2022-12-28 19:12:04 +0000

    [2022] Day 17, Part 2

--- src/main/kotlin/uk/co/mjdk/aoc22/day17/Day17.kt
+++ src/main/kotlin/uk/co/mjdk/aoc22/day17/Day17.kt
@@ -54,7 +54,7 @@ sealed interface Shape {
     }
 
     companion object {
-        fun shapeSequence(): Sequence<Shape> = listOf(Horiz, Plus, Angle, Vert, Square).repeatForever()
+        fun shapeList(): List<Shape> = listOf(Horiz, Plus, Angle, Vert, Square)
     }
 }
 
@@ -63,20 +63,27 @@ enum class Move {
     Right
 }
 
-fun getMovesSequence(): Sequence<Move> {
-    val moves = aocInput(22, 17).use { it.readText().trim() }.map { when(it) {
+fun getMovesList(): List<Move> = aocInput(22, 17).use { it.readText().trim() }.map {
+    when (it) {
         '<' -> Move.Left
         '>' -> Move.Right
         else -> throw IllegalArgumentException("Unknown char $it")
-    } }
-    return moves.repeatForever()
+    }
 }
 
 data class Pos(val row: Int, val col: Int)
 
-fun main() {
-    // make it easy, pre-allocate the board. 2022 * 4 is max of 8088 cells needed, let's do 10k to be safe
-    val board = Array(10_000) { BooleanArray(7) }
+class Board(size: Int) {
+    private val board = Array(size) { BooleanArray(7) }
+
+    var topmost: Int = -1
+        private set
+
+    var numRocks: Int = 0
+        private set
+
+    val height: Int
+        get() = topmost + 1
 
     fun collides(shape: Shape, pos: Pos): Boolean {
         if (pos.row < 0 || pos.col < 0) return true // left-bottom aligned
@@ -97,24 +104,46 @@ fun main() {
         return false
     }
 
-    // returns the topmost row baked
-    fun bake(shape: Shape, pos: Pos): Int {
-        var topmost = -1
+    fun bake(shape: Shape, pos: Pos) {
+        var topmostRow = -1
         for (shapeRow in 0..3) {
             for (shapeCol in 0..3) {
                 if (pos.col + shapeCol !in 0..6) continue
                 if (shape.cells[shapeRow][shapeCol]) {
                     board[pos.row + shapeRow][pos.col + shapeCol] = true
-                    topmost = pos.row + shapeRow
+                    topmostRow = pos.row + shapeRow
                 }
             }
         }
-        return topmost
+        topmost = max(topmost, topmostRow)
+        numRocks += 1
     }
 
-    var topmost = -1
-    val shapes = Shape.shapeSequence().iterator()
-    val moves = getMovesSequence().iterator()
+    operator fun get(row: Int, col: Int): Boolean = board[row][col]
+
+    fun simulate(nextShape: () -> Shape, nextMove: () -> Move) {
+        var pos = Pos(topmost + 4, 2)
+        val shape = nextShape()
+        while (true) {
+            // jet move
+            val jettedPos = when (nextMove()) {
+                Move.Left -> pos.copy(col = pos.col - 1)
+                Move.Right -> pos.copy(col = pos.col + 1)
+            }
+            if (!collides(shape, jettedPos)) {
+                pos = jettedPos
+            }
+
+            // downward move
+            val downwardPos = pos.copy(row = pos.row - 1)
+            if (collides(shape, downwardPos)) {
+                bake(shape, pos)
+                break
+            } else {
+                pos = downwardPos
+            }
+        }
+    }
 
     fun printBoard() {
         println("\n===== BOARD =====\n")
@@ -127,32 +156,111 @@ fun main() {
         }
         println("+-------+\n")
     }
+}
 
-    repeat(2022) {
-        var pos = Pos(topmost + 4, 2)
-        val shape = shapes.next()
-        while(true) {
-            // jet move
-            val jettedPos = when(moves.next()) {
-                Move.Left -> pos.copy(col = pos.col - 1)
-                Move.Right -> pos.copy(col = pos.col + 1)
-            }
-            if (!collides(shape, jettedPos)) {
-                pos = jettedPos
-            }
+fun part1() {
+    // make it easy, pre-allocate the board. 2022 * 4 is max of 8088 cells needed, let's do 10k to be safe
+    val board = Board(10_000)
 
-            // downward move
-            val downwardPos = pos.copy(row = pos.row - 1)
-            if (collides(shape, downwardPos)) {
-                topmost = max(topmost, bake(shape, pos))
-                break
-            } else {
-                pos = downwardPos
-            }
-        }
+    val shapes = Shape.shapeList().repeatForever().iterator()
+    val moves = getMovesList().repeatForever().iterator()
 
-        //printBoard()
+    while (board.numRocks < 2022) {
+        board.simulate(shapes::next, moves::next)
     }
 
-    println(topmost + 1)
+    println(board.height)
+}
+
+data class CacheKey(val shapeIndex: Int, val jetIndex: Int, val surface: IntArray) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as CacheKey
+
+        if (shapeIndex != other.shapeIndex) return false
+        if (jetIndex != other.jetIndex) return false
+        if (!surface.contentEquals(other.surface)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = shapeIndex
+        result = 31 * result + jetIndex
+        result = 31 * result + surface.contentHashCode()
+        return result
+    }
+}
+
+data class CacheValue(val numRocks: Int, val topmost: Int)
+
+fun part2() {
+    // Run simulation while caching the state of the surface, index of shape and index of jetstream.
+    // Cross our fingers that we detect a cycle - at that point fast-forward until the end of the simulation, then
+    // do the last bit explicitly.
+
+    val numRocksRequired = 1_000_000_000_000L
+
+    val board = Board(32_000_000) // hope the cycle is shorter than this
+
+    val cache = HashMap<CacheKey, CacheValue>()
+    val shapes = Shape.shapeList()
+    val moves = getMovesList()
+    var shapeIdx = 0
+    var moveIdx = 0
+    fun nextShape(): Shape {
+        val shape = shapes[shapeIdx]
+        shapeIdx = (shapeIdx + 1) % shapes.size
+        return shape
+    }
+
+    fun nextMove(): Move {
+        val move = moves[moveIdx]
+        moveIdx = (moveIdx + 1) % moves.size
+        return move
+    }
+
+    fun currentCacheKey(): CacheKey {
+        val surface = IntArray(7)
+        for (col in 0..6) {
+            surface[col] = (board.topmost downTo 0).firstOrNull { board[it, col] } ?: 0
+        }
+        val base = surface.min()
+        for (col in 0..6) {
+            surface[col] -= base
+        }
+        return CacheKey(shapeIdx, moveIdx, surface)
+    }
+
+    while (true) {
+        val key = currentCacheKey()
+        if (key in cache) {
+            break
+        }
+        cache[currentCacheKey()] = CacheValue(board.numRocks, board.topmost)
+        board.simulate(::nextShape, ::nextMove)
+    }
+
+    // Yay, we found a cycle. Calculate how many cycles we can get for free, and then restart the simulation to finish off
+    val key = currentCacheKey()
+    val value = cache[key]!!
+    val rocksLeft = numRocksRequired - board.numRocks
+    val cycleLength = board.numRocks - value.numRocks
+    val cycleAdvance = board.topmost - value.topmost
+    val numCycles = rocksLeft / cycleLength
+    val extraHeight = cycleAdvance.toLong() * numCycles
+    val remainingRocks = numRocksRequired - board.numRocks - (numCycles * cycleLength)
+
+    repeat(remainingRocks.toInt()) {
+        board.simulate(::nextShape, ::nextMove)
+    }
+
+    println(extraHeight + board.height)
+}
+
+fun main() {
+    part1()
+    part2()
 }
