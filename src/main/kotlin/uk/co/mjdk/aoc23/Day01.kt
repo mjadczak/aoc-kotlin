877fb377b2895fe843c3880fbaede87a9c7f8c04
Matt Jadczak <matt@mjdk.co.uk>
2023-12-02 00:03:16 +0000

    [2023] Day 1, Part 2

--- src/main/kotlin/uk/co/mjdk/aoc23/Day01.kt
+++ src/main/kotlin/uk/co/mjdk/aoc23/Day01.kt
@@ -2,19 +2,37 @@ package uk.co.mjdk.aoc23
 
 import uk.co.mjdk.aoc.aoc
 
-val input = aoc(2023, 1)
+private val input = aoc(2023, 1)
 
-fun part1() {
+private fun part1() {
     input.lineSequence().sumOf { str ->
         str.filter { it.isDigit() }.map { it.digitToInt() }.let { it.first() * 10 + it.last() }
     }.let(::println)
 }
 
-fun part2() {
+// https://stackoverflow.com/a/17971681
+private val pat = Regex("""(?=(one|two|three|four|five|six|seven|eight|nine|\d)).""")
 
+private fun (String).matchedDigit(): Int = when (this) {
+    "one" -> 1
+    "two" -> 2
+    "three" -> 3
+    "four" -> 4
+    "five" -> 5
+    "six" -> 6
+    "seven" -> 7
+    "eight" -> 8
+    "nine" -> 9
+    else -> toInt().also { check(it < 10) }
 }
 
-fun main() {
+private fun part2() {
+    input.lineSequence().sumOf { str ->
+        pat.findAll(str).map { it.groupValues[1].matchedDigit() }.let { it.first() * 10 + it.last() }
+    }.let(::println)
+}
+
+private fun main() {
     part1()
     part2()
 }
