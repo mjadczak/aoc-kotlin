d34e1c5cab6b2d6f053a86252194d0f7da3fb43e
Matt Jadczak <m.jadczak@mwam.com>
2023-12-06 13:47:05 +0000

    [2023] Day 6, Part 1

---
+++ src/main/kotlin/uk/co/mjdk/aoc23/Day06.kt
@@ -0,0 +1,51 @@
+package uk.co.mjdk.aoc23
+
+import uk.co.mjdk.aoc.aoc
+import kotlin.math.pow
+import kotlin.math.min
+import kotlin.math.max
+import kotlin.math.floor
+import kotlin.math.ceil
+import kotlin.math.sqrt
+
+private fun quadratic(a: Double, b: Double, c: Double): Pair<Double, Double> {
+    val disc = sqrt(b.pow(2).minus(4 * a * c))
+    val x1 = (-b - disc) / (2 * a)
+    val x2 = (-b + disc) / (2 * a)
+    return Pair(min(x1, x2), max(x1, x2))
+}
+
+private data class Race(val time: Int, val record: Int)
+
+private fun parseRaces(input: String): List<Race> {
+    // TODO just write a "parse whitespace-separated list of ints" function
+    val (t, r) = input.lines()
+    val ts = t.drop("Time:".length).trim().split(Regex("""\s+""")).map { it.toInt() }
+    val rs = r.drop("Distance:".length).trim().split(Regex("""\s+""")).map { it.toInt() }
+    return ts.zip(rs).map { (time, record) ->
+        Race(time, record)
+    }
+}
+
+private fun (Race).getNumBetterThanRecord(): Int {
+    // the function of distance d vs time to hold button t, where T is the max time, is
+    // d = t(T-t) = -t^2 + Tt
+    // This is always an inverted quadratic
+    // To find the number of integer times t where d > D, we find the two spots t1, t2 where the parabola = D,
+    // and find the number of the integers between t1 and t2 (not including t1 and t2)
+
+    val (t1, t2) = quadratic(-1.0, time.toDouble(), -record.toDouble())
+    return ceil(t2 - 1).toInt() - floor(t1 + 1).toInt() + 1
+}
+
+fun main() = aoc(2023, 6, ::parseRaces) {
+    example("""
+        Time:      7  15   30
+        Distance:  9  40  200
+    """.trimIndent())
+
+
+    part1 { races ->
+        races.map { it.getNumBetterThanRecord() }.reduce(Int::times)
+    }
+}
