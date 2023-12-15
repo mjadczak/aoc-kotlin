b5d7382e8a8562477f9500adc24209e0ea21f4fe
Matt Jadczak <m.jadczak@mwam.com>
2023-12-15 15:17:30 +0000

    [2023] Day 15, Part 2

--- src/main/kotlin/uk/co/mjdk/aoc23/day15/Day15.kt
+++ src/main/kotlin/uk/co/mjdk/aoc23/day15/Day15.kt
@@ -10,8 +10,60 @@ private fun String.hash(): Int = fold(0) { acc, char ->
         .let { it % 256 }
 }
 
+private class BoxMap {
+    private val buckets: List<ArrayDeque<Pair<String, Int>>> = List(256) { ArrayDeque() }
+
+    fun addLens(label: String, focalLength: Int) {
+        val bucket = buckets[label.hash()]
+        val existingIdx = bucket.indexOfFirst { it.first == label }.takeUnless { it == -1 }
+
+        if (existingIdx != null) {
+            bucket[existingIdx] = bucket[existingIdx].copy(second = focalLength)
+        } else {
+            bucket.addLast(label to focalLength)
+        }
+    }
+
+    fun removeLens(label: String) {
+        val bucket = buckets[label.hash()]
+        bucket.indexOfFirst { it.first == label }.takeUnless { it == -1 }?.let { idx ->
+            bucket.removeAt(idx)
+        }
+    }
+
+    fun focusingPower(): Int = buckets.withIndex().sumOf { bucket ->
+        bucket.value.withIndex().sumOf { lens ->
+            (bucket.index + 1) * (lens.index + 1) * lens.value.second
+        }
+    }
+
+    fun pretty() = buildString {
+        buckets.withIndex().forEach { bucket ->
+            append("Box ${bucket.index}:")
+            bucket.value.withIndex().forEach { lens ->
+                append(" [${lens.value.first} ${lens.value.second}]")
+            }
+            appendLine()
+        }
+    }
+}
+
 fun main() = aoc(2023, 15) {
     part1 { input ->
         input.splitToSequence(',').sumOf { it.hash() }
+    }
+
+    part2 { input ->
+        val boxes = BoxMap()
+        for (str in input.splitToSequence(',')) {
+            if (str.last() == '-') {
+                boxes.removeLens(str.dropLast(1))
+            } else if (str.last().isDigit()) {
+                boxes.addLens(str.dropLast(2), str.last().digitToInt())
+            } else {
+                throw IllegalStateException()
+            }
+        }
+        boxes.focusingPower()
     }
 }
