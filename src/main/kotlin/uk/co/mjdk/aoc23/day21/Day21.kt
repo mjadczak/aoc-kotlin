4c216766ac7c598d0d2a404e03722078d0927e4d
Matt Jadczak <matt@mjdk.co.uk>
2023-12-23 11:55:28 +0000

    [2023] Day 21, Part 2

--- src/main/kotlin/uk/co/mjdk/aoc23/day21/Day21.kt
+++ src/main/kotlin/uk/co/mjdk/aoc23/day21/Day21.kt
@@ -1,12 +1,15 @@
 package uk.co.mjdk.aoc23.day21
 
 import uk.co.mjdk.aoc.aoc
+import java.util.ArrayDeque
+
+private fun (Long).remainder(other: Long): Long = (this % other + other) % other
 
 private enum class Dir {
     Up, Right, Down, Left;
 }
 
-private data class Coord(val row: Int, val col: Int) {
+private data class Coord(val row: Long, val col: Long) {
     operator fun plus(dir: Dir): Coord = when (dir) {
         Dir.Up -> copy(row = row - 1)
         Dir.Down -> copy(row = row + 1)
@@ -19,13 +22,20 @@ private enum class Tile {
     Garden, Rock;
 }
 
-private class Board(private val cells: List<Tile>, val rows: Int, val start: Coord) {
+private data class Board(
+    private val cells: List<Tile>, val rows: Int, val start: Coord, val infinite: Boolean = false
+) {
     val cols = cells.size / rows
 
-    operator fun get(coord: Coord): Tile? =
-        if (coord.col in 0..<cols && coord.row in 0..<rows) cells[coord.row * cols + coord.col]
+    operator fun get(coord: Coord): Tile? = if (infinite) {
+        cells[Math.floorMod(coord.row, rows) * cols + Math.floorMod(coord.col, cols)]
+    } else {
+        if (coord.col in 0..<cols && coord.row in 0..<rows) cells[coord.row.toInt() * cols + coord.col.toInt()]
         else null
+    }
 
+
+    fun asInfinite(): Board = copy(infinite = true)
 
     companion object {
         fun parse(input: String): Board {
@@ -37,7 +47,7 @@ private class Board(private val cells: List<Tile>, val rows: Int, val start: Coo
                         '.' -> Tile.Garden
                         '#' -> Tile.Rock
                         'S' -> {
-                            start = Coord(row, col)
+                            start = Coord(row.toLong(), col.toLong())
                             Tile.Garden
                         }
 
@@ -52,23 +62,125 @@ private class Board(private val cells: List<Tile>, val rows: Int, val start: Coo
 
 private data class State(val stepsTaken: Int, val coord: Coord)
 
-private class Calculator(val board: Board, val targetSteps: Int) {
-    private val memo = mutableMapOf<State, Set<Coord>>()
+private val (Coord).parity: Boolean
+    get() = (row + col) % 2 != 0L
 
-    fun findPositions(state: State): Set<Coord> = memo[state] ?: calcPositions(state).also { memo[state] = it }
+private val (Int).parity: Boolean
+    get() = this % 2 == 1
 
-    private fun calcPositions(state: State): Set<Coord> {
-        if (state.stepsTaken == targetSteps) {
-            return setOf(state.coord)
+private fun (Triple<Int, Int, Int>).colourPrint(char: Char) {
+    val (r, g, b) = this
+    print("\u001b[38;2;")
+    print(r)
+    print(";")
+    print(g)
+    print(";")
+    print(b)
+    print("m")
+    print(char)
+    print("\u001B[0m")
+}
+
+private val red = Triple(255, 0, 0)
+private val grey = Triple(150, 150, 150)
+private val green = Triple(0, 255, 0)
+
+private fun (Board).findNumPositions(targetSteps: Int, debug: Boolean = false): Int {
+    // parity argument: we can end on any square 1) we can reach 2) has the right parity (checkerboard colour)
+    val targetParity = start.parity.let {
+        if (targetSteps.parity) !it else it
+    }
+
+    val reachable = mutableSetOf<Coord>()
+    val queue = ArrayDeque<State>()
+    queue.offer(State(0, start))
+    reachable.add(start)
+    while (queue.isNotEmpty()) {
+        val st = queue.poll()
+
+        for (dir in Dir.entries) {
+            val nextCoord = st.coord + dir
+            if (nextCoord in reachable) continue
+
+            if (this[nextCoord] == Tile.Garden) {
+                reachable.add(nextCoord)
+                val nextState = State(st.stepsTaken + 1, nextCoord)
+                if (nextState.stepsTaken == targetSteps) continue
+                queue.offer(nextState)
+            }
+        }
+    }
+
+    if (debug) {
+        println("NUM STEPS $targetSteps")
+        val diameter = 50
+        val rowr = (start.row.toInt() - diameter)..(start.row.toInt() + diameter)
+        val colr = (start.col.toInt() - diameter)..(start.col.toInt() + diameter)
+        colr.forEach { col ->
+            print(if (col == 0) 'v' else ' ')
+        }
+        println()
+        rowr.forEach { row ->
+            print(if (row == 0) '>' else ' ')
+            colr.forEach { col ->
+                val coord = Coord(row.toLong(), col.toLong())
+                if (coord in reachable && coord.parity == targetParity) {
+                    green.colourPrint(if (coord == start) 'S' else 'O')
+                } else if (coord in reachable) {
+                    grey.colourPrint(if (coord == start) 's' else 'o')
+                } else if (this[coord] == Tile.Rock) red.colourPrint('#')
+                else grey.colourPrint('.')
+            }
+            println()
         }
 
-        return Dir.entries.asSequence().map { state.coord + it }.filter { board[it] == Tile.Garden }
-            .map { findPositions(State(state.stepsTaken + 1, it)) }.reduce { l, r -> l.union(r) }
+        println()
     }
+
+    return reachable.count { it.parity == targetParity }
 }
 
 fun main() = aoc(2023, 21, { Board.parse(it) }) {
+    example(
+        """
+        ...........
+        .....###.#.
+        .###.##..#.
+        ..#.#...#..
+        ....#.#....
+        .##..S####.
+        .##..#...#.
+        .......##..
+        .##.#.####.
+        .##..##.##.
+        ...........
+    """.trimIndent()
+    )
+
     part1 { board ->
-        Calculator(board, 64).findPositions(State(0, board.start)).size
+        board.findNumPositions(64)
+    }
+
+    part2 { srcBoard ->
+        val board = srcBoard.asInfinite()
+
+        val target = 26_501_365
+
+        // meh - special structure of input lends itself to this solution
+        val size = board.cols
+        check(board.rows == size)
+        val edge = size / 2
+        // this ends up a quadratic - so grab the three points and calculate the solution at the end
+        val y = (0..2).map { i -> edge + i * size }.map { board.findNumPositions(it).toLong() }
+        val c = y[0]
+        val a = (y[2] - (2 * y[1]) + c) / 2
+        val b = y[1] - c - a
+
+        val x = (target - edge) / size
+        check((target - edge) % size == 0)
+
+        println("y=$y, a=$a, b=$b, c=$c, x=$x")
+
+        (a * x * x) + (b * x) + c
     }
 }
