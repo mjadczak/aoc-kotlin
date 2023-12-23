e448b24eda778d242efda6e2aea09e6bc784858f
Matt Jadczak <matt@mjdk.co.uk>
2023-12-23 20:39:54 +0000

    [2023] Day 23, Part 2

--- src/main/kotlin/uk/co/mjdk/aoc23/day23/Day23.kt
+++ src/main/kotlin/uk/co/mjdk/aoc23/day23/Day23.kt
@@ -2,9 +2,8 @@ package uk.co.mjdk.aoc23.day23
 
 import kotlinx.collections.immutable.PersistentSet
 import kotlinx.collections.immutable.persistentHashSetOf
-import kotlinx.collections.immutable.persistentSetOf
 import uk.co.mjdk.aoc.aoc
-import java.util.ArrayDeque
+import java.util.*
 import kotlin.math.max
 
 private enum class Dir {
@@ -18,6 +17,8 @@ private data class Coord(val row: Int, val col: Int) {
         Dir.Left -> copy(col = col - 1)
         Dir.Right -> copy(col = col + 1)
     }
+
+    override fun toString(): String = "($row,$col)"
 }
 
 private sealed interface Tile
@@ -38,6 +39,7 @@ private val Tile.allowedDirs: Set<Dir>
 private class Board(private val cells: List<Tile>, val rows: Int) {
     val cols = cells.size / rows
     val start = Coord(0, (0..<cols).first { col -> this[Coord(0, col)] == Path })
+    val target = Coord(rows - 1, (0..<cols).first { col -> this[Coord(rows - 1, col)] == Path })
 
     operator fun get(coord: Coord): Tile? =
         if (coord.col in 0..<cols && coord.row in 0..<rows) cells[coord.row * cols + coord.col]
@@ -67,6 +69,36 @@ private class Board(private val cells: List<Tile>, val rows: Int) {
 
 
 fun main() = aoc(2023, 23, { Board.parse(it) }) {
+    // initially, I absolutely missed that we need to reach the end of the board!
+
+    example(
+        """
+        #.#####################
+        #.......#########...###
+        #######.#########.#.###
+        ###.....#.>.>.###.#.###
+        ###v#####.#v#.###.#.###
+        ###.>...#.#.#.....#...#
+        ###v###.#.#.#########.#
+        ###...#.#.#.......#...#
+        #####.#.#.#######.#.###
+        #.....#.#.#.......#...#
+        #.#####.#.#.#########v#
+        #.#...#...#...###...>.#
+        #.#.#v#######v###.###v#
+        #...#.>.#...>.>.#.###.#
+        #####v#.#.###v#.#.###.#
+        #.....#...#...#.#.#...#
+        #.#########.###.#.#.###
+        #...###...#...#...#.###
+        ###.###.#.###v#####v###
+        #...#...#.#.>.>.#.>.###
+        #.###.###.#.###.#.#v###
+        #.....###...###...#...#
+        #####################.#
+    """.trimIndent()
+    )
+
     part1 { board ->
         data class State(val coord: Coord, val visited: PersistentSet<Coord>) {
             val length: Int
@@ -85,13 +117,91 @@ fun main() = aoc(2023, 23, { Board.parse(it) }) {
             if (tile == null || tile == Forest) {
                 continue
             }
-            maxDistance = max(maxDistance, st.length)
-            val nextVisited = st.visited.add(st.coord)
-            tile.allowedDirs.asSequence().map { st.coord + it }.filterNot { it in st.visited }.forEach {
-                queue.offer(State(it, nextVisited))
+            if (st.coord == board.target) {
+                maxDistance = max(maxDistance, st.length)
+            } else {
+                val nextVisited = st.visited.add(st.coord)
+                tile.allowedDirs.asSequence().map { st.coord + it }.filterNot { it in st.visited }.forEach {
+                    queue.offer(State(it, nextVisited))
+                }
             }
         }
 
+
+        maxDistance
+    }
+
+    part2 { board ->
+        // Large parts of the graph only have one way to go, simplify those down
+        // We only store the max distance
+        val edges = mutableMapOf<Coord, MutableMap<Coord, Int>>()
+
+        fun addEdge(from: Coord, to: Coord, distance: Int) {
+            edges.computeIfAbsent(from) { mutableMapOf() }.let { map -> map[to] = max(map[to] ?: 0, distance) }
+            edges.computeIfAbsent(to) { mutableMapOf() }.let { map -> map[from] = max(map[from] ?: 0, distance) }
+        }
+
+        fun Coord.neighbours(): Set<Coord> =
+            Dir.entries.asSequence().map { this + it }.filter { c -> board[c].let { it == Path || it is Slope } }
+                .toSet()
+
+        val poles = (0..<board.rows).asSequence().flatMap { row ->
+            (0..<board.cols).mapNotNull { col ->
+                val coord = Coord(row, col)
+                val tile = board[coord]!!
+                if (tile == Forest) {
+                    null
+                } else {
+                    val neighbours = coord.neighbours()
+                    if (neighbours.size == 2) {
+                        null
+                    } else {
+                        coord to neighbours
+                    }
+                }
+            }
+        }.toMap()
+        check(board.start in poles)
+        check(board.target in poles)
+
+        // from each pole, we have some set of directions (1 or 3+), and if we follow them we should have a straight path until we encounter another pole
+
+        poles.forEach { (startPole, nextCoords) ->
+            nextCoords.forEach { nextCoord ->
+                // we could probably just detect the poles inline and do the whole thing with a single DFS - but meh
+                // we end up traversing each path both ways, oh well
+                var distance = 1
+                var last = startPole
+                var current = nextCoord
+                while (current !in poles) {
+                    val next = current.neighbours().first { it != last }
+                    last = current
+                    current = next
+                    distance += 1
+                }
+                addEdge(startPole, current, distance)
+            }
+        }
+
+        // now a lil' recursive DFS without needing to store many sets, just for fun
+        val visited = linkedSetOf<Coord>()
+        var maxDistance = 0
+
+        fun visit(node: Coord, distance: Int) {
+            if (node == board.target) {
+                maxDistance = max(maxDistance, distance)
+                return
+            }
+
+            visited.add(node)
+            val nextNodes = edges[node]!!.entries.filter { it.key !in visited }
+            nextNodes.forEach { (next, dist) ->
+                visit(next, distance + dist)
+            }
+            visited.remove(node)
+        }
+
+        visit(board.start, 0)
 
         maxDistance
     }
