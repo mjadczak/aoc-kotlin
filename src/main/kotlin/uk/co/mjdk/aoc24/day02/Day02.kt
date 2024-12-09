6f1d2b87a76aa5c01e28765bb26b86a65566dea2
Matt Jadczak <m.jadczak@mwam.com>
2024-12-09 11:53:43 +0000

    [2024] Day 2, Part 2

--- src/main/kotlin/uk/co/mjdk/aoc24/day02/Day02.kt
+++ src/main/kotlin/uk/co/mjdk/aoc24/day02/Day02.kt
@@ -4,13 +4,24 @@ import uk.co.mjdk.aoc.aoc
 import kotlin.math.absoluteValue
 import kotlin.math.sign
 
+fun isValid(list: List<Int>): Boolean {
+    val diffs = list.zipWithNext { a, b -> b - a }
+    val signsSame = diffs.map { it.sign }.distinct().size == 1
+    val sizesOk = diffs.all { it.absoluteValue in 1..3 }
+    return signsSame && sizesOk
+}
+
 fun main() = aoc(2024, 2, { it.lines().map { line -> line.split(" ").map { num -> num.toInt() } } }) {
     part1 { lists ->
         lists.count { list ->
-            val diffs = list.zipWithNext { a, b -> b - a }
-            val signsSame = diffs.map { it.sign }.distinct().size == 1
-            val sizesOk = diffs.all { it.absoluteValue in 1..3 }
-            signsSame && sizesOk
+            isValid(list)
+        }
+    }
+
+    part2 { lists ->
+        // bruteforce - there's definitely a smarter way!
+        lists.count { list ->
+            isValid(list) || list.indices.any { badIdx -> isValid(list.filterIndexed { index, _ -> index != badIdx }) }
         }
     }
 }
