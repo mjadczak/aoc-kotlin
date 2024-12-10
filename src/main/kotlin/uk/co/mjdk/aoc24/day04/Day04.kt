e8d3c34bda750924a57c56957934141d415f78e7
Matt Jadczak <matt@mjdk.co.uk>
2024-12-10 13:28:27 +0000

    [2024] Day 4, Part 2

--- src/main/kotlin/uk/co/mjdk/aoc24/day04/Day04.kt
+++ src/main/kotlin/uk/co/mjdk/aoc24/day04/Day04.kt
@@ -22,6 +22,21 @@ fun countXmasOccurrences(cs: CharSequence): Int {
     return count
 }
 
+// returns the indexes with the middle A
+fun findMasOccurrences(cs: CharSequence): List<Int> =
+    buildList {
+        cs.forEachIndexed { idx, char ->
+            if (char == 'M') {
+                if (idx + 3 <= cs.length && cs.subSequence(idx, idx + 3) == "MAS") {
+                    add(idx + 1)
+                }
+                if (idx >= 2 && cs.subSequence(idx - 2, idx + 1) == "SAM") {
+                    add(idx - 1)
+                }
+            }
+        }
+    }
+
 class CharSubSequence(val cs: CharSequence, val start: Int, val end: Int) : CustomCharSequence() {
     override val length: Int = end - start
     override fun get(index: Int): Char = cs[start + index]
@@ -35,6 +50,8 @@ class CharSubSequence(val cs: CharSequence, val start: Int, val end: Int) : Cust
         require(cs.length >= length)
     }
 }
+
+data class Coord(val row: Int, val col: Int)
 
 abstract class CustomCharSequence : CharSequence {
     override fun subSequence(startIndex: Int, endIndex: Int): CharSequence = CharSubSequence(this, startIndex, endIndex)
@@ -80,7 +97,7 @@ class Board(private val lines: Array<CharArray>) {
             }
         }
 
-    private inner class DiagCharSequence(maxDiagLen: Int, squareLen: Int, diagNum: Int, val topLeft: Boolean) :
+    inner class DiagCharSequence(maxDiagLen: Int, squareLen: Int, diagNum: Int, val topLeft: Boolean) :
         CustomCharSequence() {
         val diagLen = min(squareLen, maxDiagLen)
         val startRow = min(diagNum, rows - 1)
@@ -90,12 +107,17 @@ class Board(private val lines: Array<CharArray>) {
             get() = diagLen
 
         override fun get(index: Int): Char {
+            val coord = coordOfIndex(index)
+            return lines[coord.row][coord.col]
+        }
+
+        fun coordOfIndex(index: Int): Coord {
             val colIdx = if (topLeft) startCol + index else startCol - index
-            return lines[startRow - index][colIdx]
+            return Coord(startRow - index, colIdx)
         }
     }
 
-    private fun diagSequence(topLeft: Boolean): Sequence<CharSequence> = sequence {
+    private fun diagSequence(topLeft: Boolean): Sequence<DiagCharSequence> = sequence {
         val numDiags = rows + cols - 1
         val maxDiagLen = min(rows, cols)
         var squareLen = 1
@@ -109,10 +131,10 @@ class Board(private val lines: Array<CharArray>) {
         }
     }
 
-    val diag1: Sequence<CharSequence>
+    val diag1: Sequence<DiagCharSequence>
         get() = diagSequence(true)
 
-    val diag2: Sequence<CharSequence>
+    val diag2: Sequence<DiagCharSequence>
         get() = diagSequence(false)
 }
 
@@ -124,5 +146,15 @@ fun main() = aoc(2024, 4, { input -> Board(input.lines().map { it.toCharArray() 
             board.diag1,
             board.diag2,
         ).sumOf { seqs -> seqs.sumOf { countXmasOccurrences(it) } }
+    }
+
+    part2 { board ->
+        val leftCoords = board.diag1.flatMap { diag ->
+            findMasOccurrences(diag).map { diag.coordOfIndex(it) }
+        }.toSet()
+        val rightCoords = board.diag2.flatMap { diag ->
+            findMasOccurrences(diag).map { diag.coordOfIndex(it) }
+        }.toSet()
+        leftCoords.intersect(rightCoords).size
     }
 }
