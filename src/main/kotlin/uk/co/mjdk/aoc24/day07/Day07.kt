439693c549da60156373542998c57fb3c1cdf585
Matt Jadczak <matt@mjdk.co.uk>
2024-12-10 23:37:47 +0000

    [2024] Day 7, Part 2

--- src/main/kotlin/uk/co/mjdk/aoc24/day07/Day07.kt
+++ src/main/kotlin/uk/co/mjdk/aoc24/day07/Day07.kt
@@ -1,15 +1,26 @@
 package uk.co.mjdk.aoc24.day07
 
 import uk.co.mjdk.aoc.aoc
+import kotlin.math.floor
+import kotlin.math.log10
+import kotlin.math.pow
+
+fun Long.concat(other: Long): Long {
+    val numDigits = floor(log10(other.toDouble()) + 1.0)
+    return this * (10.0.pow(numDigits).toLong()) + other
+}
 
 data class Equation(val result: Long, val inputs: List<Long>) {
-    fun isValid(): Boolean {
+    fun isValid(allowConcat: Boolean): Boolean {
         fun check(total: Long, remaining: List<Long>): Boolean {
             if (total > result) return false
             if (remaining.isEmpty()) return total == result
             val next = remaining.first()
             val remaining2 = remaining.subList(1, remaining.size)
-            return check(total + next, remaining2) || check(total * next, remaining2)
+            return check(total + next, remaining2) || check(
+                total * next,
+                remaining2
+            ) || (allowConcat && check(total.concat(next), remaining2))
         }
 
         return check(inputs.first(), inputs.subList(1, inputs.size))
@@ -26,6 +37,10 @@ data class Equation(val result: Long, val inputs: List<Long>) {
 
 fun main() = aoc(2024, 7, { it.lines().map(Equation::parse) }) {
     part1 { eqns ->
-        eqns.filter { it.isValid() }.sumOf { it.result }
+        eqns.filter { it.isValid(false) }.sumOf { it.result }
+    }
+
+    part2 { eqns ->
+        eqns.filter { it.isValid(true) }.sumOf { it.result }
     }
 }
