90f390237a482954e1717933c9d9c893930bfee6
Matt Jadczak <m.jadczak@mwam.com>
2025-12-03 09:15:02 +0000

    [2025] Day 3, Part 2

--- src/main/kotlin/uk/co/mjdk/aoc25/day03/Day03.kt
+++ src/main/kotlin/uk/co/mjdk/aoc25/day03/Day03.kt
@@ -5,6 +5,17 @@ import me.alllex.parsus.token.literalToken
 import me.alllex.parsus.token.regexToken
 import uk.co.mjdk.aoc.aoc
 
+fun pow10(power: Int): Long {
+    check(power >= 0)
+    var res = 1L
+    var pow = power
+    while (pow > 0) {
+        res *= 10
+        pow -= 1
+    }
+    return res
+}
+
 fun main() = aoc(2025, 3, object : Grammar<List<List<Int>>>() {
     val digit by regexToken("\\d").map { it.text.toInt() }
     val nl by literalToken("\n")
@@ -31,6 +42,33 @@ fun main() = aoc(2025, 3, object : Grammar<List<List<Int>>>() {
             val first = greedyDigits.first { it.index != bank.indices.last }
             val second = greedyDigits.first { it.index > first.index }
             first.value * 10 + second.value
+        }
+    }
+
+    part2 { banks ->
+        banks.sumOf { bank ->
+            val maxNumDigits = 12
+            val totalNumDigits = bank.size
+            check(maxNumDigits <= totalNumDigits)
+            // biggest number of [n] digits, which can be formed using numbers from indices [m]..<s
+            val memo = List(maxNumDigits + 1) {
+                MutableList(totalNumDigits) { null as Long? }
+            }
+            for (numDigits in 1..maxNumDigits) {
+                var maximalDigit = 0
+                var maximalIndex = -1
+                for (leftmostIdx in (totalNumDigits - numDigits) downTo maxNumDigits - numDigits) {
+                    val digit = bank[leftmostIdx]
+                    if (digit >= maximalDigit) {
+                        maximalDigit = digit
+                        maximalIndex = leftmostIdx
+                    }
+                    val maxRestValue = if (numDigits == 1) 0L else memo[numDigits - 1][maximalIndex + 1]!!
+                    val total = maximalDigit * pow10(numDigits - 1) + maxRestValue
+                    memo[numDigits][leftmostIdx] = total
+                }
+            }
+            memo[maxNumDigits][0]!!
         }
     }
 }
